1d4a261fef77af20556f262650d5dbef7863fe80
afortier12 <afortier12@gmail.com>
2020-06-01 15:52:14 -0400

    Added UML for parts and storage

--- app/src/main/java/com/example/fiix_custom_mobile/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/fiix_custom_mobile/ui/dashboard/DashboardFragment.java
@@ -10,7 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.fiix_custom_mobile.R;
 
@@ -21,7 +21,7 @@ public class DashboardFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel.class);
+                new ViewModelProvider(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
         final TextView textView = root.findViewById(R.id.text_dashboard);
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
