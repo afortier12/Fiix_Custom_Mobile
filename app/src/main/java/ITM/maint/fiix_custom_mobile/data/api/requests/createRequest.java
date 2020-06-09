2b46eec711bdb391374c3052c1217336d687e06a
afortier12 <afortier12@gmail.com>
2020-06-08 21:20:08 -0400

    added api folders

---
+++ app/src/main/java/ITM/maint/fiix_custom_mobile/data/api/requests/createRequest.java
@@ -0,0 +1,4 @@
+package ITM.maint.fiix_custom_mobile.data.api.requests;
+
+public class createRequest {
+}
---
+++ app/src/main/java/ITM/maint/fiix_custom_mobile/data/repository/create.java
@@ -0,0 +1,4 @@
+package ITM.maint.fiix_custom_mobile.data.repository;
+
+public class create {
+}
